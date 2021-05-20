93ec536fb974951446e199426968c3efc969a5d3
Lina Rusakova <69868318+LinaRusakova@users.noreply.github.com>
2021-05-20 23:59:57 +0300

    start making new changes

--- src/main/java/ru/geekbrains/spring/boot/april/market/utils/Cart.java
+++ src/main/java/ru/geekbrains/spring/boot/april/market/utils/Cart.java
@@ -4,7 +4,9 @@ package ru.geekbrains.spring.boot.april.market.utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.geekbrains.spring.boot.april.market.dtos.ProductDto;
+import ru.geekbrains.spring.boot.april.market.error_handling.ResourceNotFoundException;
 import ru.geekbrains.spring.boot.april.market.models.Product;
+import ru.geekbrains.spring.boot.april.market.services.ProductService;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -13,6 +15,21 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class Cart {
+    private final ProductService productService;
+    private List<Product> items;
+    private int sum;
+
+    public void addToCart(Long id) {
+        Product product=productService.findProductByID(id).orElseThrow(() -> new ResourceNotFoundException("Product doesn't exist with id: " + id + ". An error occurred while executing the procedure: adding an item to the cart. "));
+        items.add(product);
+    }
+
+    private void recalculate() {
+        sum = 0;
+        for (Product product : items) {
+            sum += product.getPrice();
+        }
+    }
 
     public List<Product> getItems() {
         return items;
@@ -41,7 +58,6 @@ public class Cart {
         return result;
     }
 
-    private List<Product> items;
 
     @PostConstruct
     public void init() {
